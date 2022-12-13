c6e2b2c2b860bb9fa6cd29a46a335bbf8b385da3
Keyang.Liu <stephenkobylky@gmail.com>
2022-12-13 18:31:36 +0900

    Add commitment and proof check

--- fks-dict/tests/tests.rs
+++ fks-dict/tests/tests.rs
@@ -14,25 +14,50 @@ mod tests {
     #[case(1000, 100000)]
     #[case(10000, 100000)]
     fn random_u32_list_tests(#[case] num: u32, #[case] max: u32) {
-        for _ in 1 ..=10 {
+        for _ in 1..=10 {
             println!("Generate test set");
             let set = gen_rand_list(num, max);
             println!("Generate FKS dict");
             let dict = fks_dict::FKSDict::new(&set, max);
             println!("Start Querying");
-            for x in 1 ..=max {
+            for x in 1..=max {
                 assert_eq!(dict.query(x), set.contains(&x));
             }
         }
     }
 
-    fn gen_rand_list(num: u32, max: u32) -> HashSet<u32>{
+    #[rstest]
+    #[case(10, 500)]
+    #[case(100, 500)]
+    #[case(10, 1000)]
+    #[case(100, 1000)]
+    #[case(200, 1000)]
+    fn random_u32_list_proof_tests(#[case] num: u32, #[case] max: u32) {
+        println!("Generate test set");
+        let set = gen_rand_list(num, max);
+        println!("Generate FKS dict");
+        let dict = fks_dict::FKSDict::new(&set, max);
+        println!("Start Querying");
+        let refer = vec![num, max];
+        for x in 1..=max {
+            let commit = dict.commit(&refer);
+            let proof = dict.gen_proof(x, &refer);
+            assert!(fks_dict::verify_commit_proof(
+                x,
+                &refer,
+                &proof,
+                commit,
+                set.contains(&x)
+            ));
+        }
+    }
 
+    fn gen_rand_list(num: u32, max: u32) -> HashSet<u32> {
         let mut rng = rand::thread_rng();
-        let mut set:HashSet<u32> = HashSet::new();
+        let mut set: HashSet<u32> = HashSet::new();
 
         while set.len() < num as usize {
-            set.insert(rng.gen_range(1 ..max));
+            set.insert(rng.gen_range(1..max));
         }
 
         return set;
