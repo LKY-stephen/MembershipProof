5808cd802a7f53a293cd5f803402d023edf06766
Keyang.Liu <stephenkobylky@gmail.com>
2023-04-04 00:29:38 +0900

    implement circuit for extended merkle path

---
+++ psc/src/chip.rs
@@ -0,0 +1,479 @@
+// Since we need to verify the hash computation
+// The constraints will be an extension to the poseidon_chip's
+// with additional merkle related constraints.
+
+use std::{marker::PhantomData, vec};
+
+use ff::PrimeField;
+use halo2_proofs::{
+    circuit::{AssignedCell, Chip, Layouter, Region, Value},
+    plonk::{
+        Advice, Column, ConstraintSystem, Constraints, Error,
+        Expression::{self},
+        Instance, Selector,
+    },
+    poly::Rotation,
+};
+
+#[derive(Debug, Clone)]
+pub struct Node<F: PrimeField, const I: usize>([AssignedCell<F, F>; I]);
+
+pub trait MerkleExtendedPathInstruction<F: PrimeField, const I: usize>: Chip<F> {
+    /// Variable representing a tree node
+    type Node;
+
+    /// Loads a left child, a right child and paths
+    /// copy only works from b to m
+    fn load_path(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        left: Vec<[AssignedCell<F, F>; I]>,
+        right: Vec<[AssignedCell<F, F>; I]>,
+        hash: Vec<[AssignedCell<F, F>; I]>,
+        copy: &Vec<Value<F>>,
+        m: usize,
+        b: usize,
+    ) -> Result<(), Error>;
+
+    /// Loads a left child, a right child
+    /// return a node of its selection according to
+    /// index
+    fn load_leaves(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        left: [AssignedCell<F, F>; I],
+        right: [AssignedCell<F, F>; I],
+    ) -> Result<(), Error>;
+
+    /// check the final result with index
+    fn expose_public(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        num: Self::Node,
+        row: usize,
+    ) -> Result<(), Error>;
+}
+
+pub struct MerkleExtendedPathChip<F: PrimeField, const I: usize> {
+    config: MerklePathConfig<I>,
+    _marker: PhantomData<F>,
+}
+
+///
+/// The chip handles three
+#[derive(Clone, Debug)]
+pub struct MerklePathConfig<const I: usize> {
+    /// private input for element
+    value: [Column<Advice>; I],
+
+    /// flag for hash and copy
+    copy_flag: Column<Advice>,
+
+    /// flag for left child or right child
+    index_flag: Column<Advice>,
+
+    /// This is the public input (instance) column.
+    public: Column<Instance>,
+
+    /// selector for hash query
+    s_bucket: Selector,
+
+    /// selector for copy_hash query
+    s_chash: Selector,
+
+    /// selector for hash query
+    s_pub: Selector,
+}
+
+impl<F: PrimeField, const I: usize> MerkleExtendedPathChip<F, I> {
+    pub fn configure(
+        meta: &mut ConstraintSystem<F>,
+        value: [Column<Advice>; I],
+        copy_flag: Column<Advice>,
+        index_flag: Column<Advice>,
+        public: Column<Instance>,
+    ) -> <Self as Chip<F>>::Config {
+        // equality checks for output and internal states
+        for i in 0..I {
+            meta.enable_equality(value[i]);
+        }
+        meta.enable_equality(public);
+        meta.enable_equality(index_flag);
+
+        let s_hash = meta.selector();
+        let s_pub = meta.selector();
+        let s_chash = meta.selector();
+
+        let one = Expression::Constant(F::ONE);
+        let bool_constraint = |v: Expression<F>| v.clone() * (one.clone() - v);
+
+        // from layer b, we may copy the path for some times, therefore, we use
+        // the copy row to identify which row we are copying.
+        // the last n row do not need to copy, hence the copy flag should be
+        // 1 for b to m-n rows
+        let copy_flag_constraint =
+            |before: Expression<F>, after: Expression<F>| after * (one.clone() - before);
+        // constraints the hash and copy constraints
+        meta.create_gate("Bucket_Hash", |meta| {
+            let s_hash = meta.query_selector(s_hash);
+
+            // we store values as
+            // value         copy      index      s_hash
+            // left           -          -          0
+            // right          -          -          0
+            // hash           -         0/1         1
+            // next left      -          -          0
+            // next right     -          -          0
+
+            let p_hash_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation::cur()))
+                .collect::<Vec<_>>();
+
+            let index = meta.query_advice(index_flag, Rotation::cur());
+
+            let left_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation::next()))
+                .collect::<Vec<_>>();
+
+            let right_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation(2)))
+                .collect::<Vec<_>>();
+
+            // (1-copy)*(p_hash - (1-index)*left -index * right) is the hash constraint
+
+            let hash_constraint = (0..I)
+                .map(|i| {
+                    p_hash_v[i].clone()
+                        - (one.clone() - index.clone()) * left_v[i].clone()
+                        - index.clone() * right_v[i].clone()
+                })
+                .collect::<Vec<_>>();
+
+            let constraints = vec![bool_constraint(index)]
+                .into_iter()
+                .chain(hash_constraint);
+            Constraints::with_selector(s_hash, constraints)
+        });
+        // constraints the hash and copy constraints
+        meta.create_gate("Copy_Hash", |meta| {
+            let s_chash = meta.query_selector(s_chash);
+
+            // we store values as
+            // value         copy      index      s_hash
+            // left           -          -          0
+            // right          -          -          0
+            // hash          0/1        0/1         1
+            // next left      -          -          0
+            // next right     -          -          0
+
+            let p_left_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation(-2)))
+                .collect::<Vec<_>>();
+
+            let p_right_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation::prev()))
+                .collect::<Vec<_>>();
+
+            let p_hash_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation::cur()))
+                .collect::<Vec<_>>();
+
+            let n_copy = meta.query_advice(copy_flag, Rotation(3));
+            let copy = meta.query_advice(copy_flag, Rotation::cur());
+            let index = meta.query_advice(index_flag, Rotation::cur());
+
+            let left_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation::next()))
+                .collect::<Vec<_>>();
+
+            let right_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation(2)))
+                .collect::<Vec<_>>();
+
+            // copy is zero until some point it becomes one (p_copy*(1-copy)).
+            // index is bool value
+            // (1-copy)*(p_hash - (1-index)*left -index * right) is the hash constraint
+            // copy*((1-index)*(p_left-left) + (index) (p_right - right)) is the copy constraint
+
+            let hash_constraint = (0..I)
+                .map(|i| {
+                    (one.clone() - copy.clone())
+                        * (p_hash_v[i].clone()
+                            - (one.clone() - index.clone()) * left_v[i].clone()
+                            - index.clone() * right_v[i].clone())
+                })
+                .collect::<Vec<_>>();
+            let copy_constraint = (0..I)
+                .map(|i| {
+                    copy.clone()
+                        * ((one.clone() - index.clone())
+                            * (p_left_v[i].clone() - left_v[i].clone())
+                            + index.clone() * (p_right_v[i].clone() - right_v[i].clone()))
+                })
+                .collect::<Vec<_>>();
+
+            let constraints = vec![
+                bool_constraint(n_copy.clone()),
+                bool_constraint(copy.clone()),
+                copy_flag_constraint(copy.clone(), n_copy.clone()),
+                bool_constraint(index),
+            ]
+            .into_iter()
+            .chain(hash_constraint)
+            .chain(copy_constraint);
+            Constraints::with_selector(s_chash, constraints)
+        });
+
+        // constraints the hash and copy for the first layer inputs
+        meta.create_gate("PUB_SELECT", |meta| {
+            let s_pub = meta.query_selector(s_pub);
+
+            // we store values as
+            // value         copy      index      s_pub
+            // left           -          -          0
+            // right          -          -          0
+            // hash          0/1        0/1         1
+
+            let left_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation(-2)))
+                .collect::<Vec<_>>();
+
+            let right_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation::prev()))
+                .collect::<Vec<_>>();
+
+            let hash_v = (0..I)
+                .map(|i| meta.query_advice(value[i], Rotation::cur()))
+                .collect::<Vec<_>>();
+
+            let index = meta.query_advice(index_flag, Rotation::cur());
+            let copy = meta.query_advice(copy_flag, Rotation::cur());
+
+            // copy left to hash if index is 0 other wise copy the right one
+
+            let copy_constraint = (0..I)
+                .map(|i| {
+                    (one.clone() - index.clone()) * (left_v[i].clone() - hash_v[i].clone())
+                        + index.clone() * (right_v[i].clone() - hash_v[i].clone())
+                })
+                .collect::<Vec<_>>();
+
+            let constraints = vec![bool_constraint(index.clone()), copy.clone()]
+                .into_iter()
+                .chain(copy_constraint);
+            Constraints::with_selector(s_pub, constraints)
+        });
+
+        MerklePathConfig {
+            value,
+            public,
+            copy_flag,
+            index_flag,
+            s_bucket: s_hash,
+            s_pub,
+            s_chash,
+        }
+    }
+
+    /// Construct a [`Pow5Chip`].
+    pub fn construct(config: MerklePathConfig<I>) -> Self {
+        MerkleExtendedPathChip {
+            config,
+            _marker: PhantomData,
+        }
+    }
+}
+
+impl<F: PrimeField, const I: usize> MerkleExtendedPathInstruction<F, I>
+    for MerkleExtendedPathChip<F, I>
+{
+    type Node = Node<F, I>;
+
+    fn load_path(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        left: Vec<[AssignedCell<F, F>; I]>,
+        right: Vec<[AssignedCell<F, F>; I]>,
+        hash: Vec<[AssignedCell<F, F>; I]>,
+        copy: &Vec<Value<F>>,
+        m: usize,
+        b: usize,
+    ) -> Result<(), Error> {
+        let config = self.config();
+        assert_eq!(m, right.len());
+        assert_eq!(m, left.len());
+        assert_eq!(m, copy.len());
+        assert_eq!(m, hash.len());
+
+        layouter.assign_region(
+            || "load path",
+            |mut region: Region<'_, F>| {
+                // from first m-1 rows we do the hash copy check
+                //
+                // |  value  | copy | index| s_hash|
+                // |  left1  |  *   |  *   |    0  |
+                // |  right1 |  *   |  *   |    0  |
+                // |  hash1  |  0   |  0   |    1  |
+                // ....
+                // hash(i) =  left(i+1) if index =0 else right(i+1)
+
+                for i in 0..(m - 1) {
+                    let cur_pos = i * 3;
+                    let hash_pos = cur_pos + 2;
+                    for j in 0..I {
+                        left[i][j].copy_advice(
+                            || "assign left",
+                            &mut region,
+                            config.value[j],
+                            cur_pos,
+                        )?;
+                        right[i][j].copy_advice(
+                            || "assign right",
+                            &mut region,
+                            config.value[j],
+                            cur_pos + 1,
+                        )?;
+                        hash[i][j].copy_advice(
+                            || "copy hash",
+                            &mut region,
+                            config.value[j],
+                            hash_pos,
+                        )?;
+                    }
+
+                    if i < (b - 1) {
+                        // first b row, we use hash for sure
+                        config.s_bucket.enable(&mut region, hash_pos)?;
+                    } else {
+                        config.s_chash.enable(&mut region, hash_pos)?;
+                    }
+
+                    region.assign_advice(
+                        || "assign copy",
+                        config.copy_flag,
+                        hash_pos,
+                        || copy[i],
+                    )?;
+
+                    region.assign_advice_from_instance(
+                        || "assign index",
+                        config.public,
+                        I + i + 1,
+                        config.index_flag,
+                        hash_pos,
+                    )?;
+                }
+
+                // for the final row, we do not check index further but we do instance check
+
+                let last_pos = (m - 1) * 3;
+                for j in 0..I {
+                    left[m - 1][j]
+                        .copy_advice(|| "assign left", &mut region, config.value[j], last_pos)
+                        .unwrap();
+                    right[m - 1][j]
+                        .copy_advice(
+                            || "assign right",
+                            &mut region,
+                            config.value[j],
+                            last_pos + 1,
+                        )
+                        .unwrap();
+                }
+
+                // assign next copy for the copy check
+                region.assign_advice(
+                    || "assign copy",
+                    config.copy_flag,
+                    last_pos + 2,
+                    || Value::known(F::ZERO),
+                )?;
+
+                return Ok(());
+            },
+        )
+    }
+
+    fn expose_public(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        num: Self::Node,
+        row: usize,
+    ) -> Result<(), Error> {
+        let config = self.config();
+
+        for i in 0..I {
+            layouter.constrain_instance(num.0[i].cell(), config.public, row + i)?;
+        }
+        Ok(())
+    }
+
+    fn load_leaves(
+        &self,
+        layouter: &mut impl Layouter<F>,
+        left: [AssignedCell<F, F>; I],
+        right: [AssignedCell<F, F>; I],
+    ) -> Result<(), Error> {
+        let config = self.config();
+
+        layouter
+            .assign_region(
+                || "load inputs",
+                |mut region: Region<'_, F>| {
+                    // pub copy layer
+                    // |  value  | copy | index|  s_pub|
+                    // |  left   |  *   |   *  |    0  |
+                    // |  right  |  *   |   *  |    0  |
+                    // |  chosen |  0   |  0/1 |    1  |
+                    // ....
+                    // chosen = pub1,pub2, ... pubI
+
+                    config.s_pub.enable(&mut region, 2)?;
+                    for j in 0..I {
+                        left[j].copy_advice(|| "assign left", &mut region, config.value[j], 0)?;
+                        right[j].copy_advice(|| "assign right", &mut region, config.value[j], 1)?;
+                        region.assign_advice_from_instance(
+                            || "copy selected leaf from instance",
+                            config.public,
+                            j,
+                            config.value[j],
+                            2,
+                        )?;
+                    }
+
+                    region.assign_advice_from_instance(
+                        || "assign index for zero layer",
+                        config.public,
+                        I,
+                        config.index_flag,
+                        2,
+                    )?;
+
+                    region.assign_advice(
+                        || "assign copy",
+                        config.copy_flag,
+                        2,
+                        || Value::known(F::ZERO),
+                    )?;
+
+                    Ok(())
+                },
+            )
+            .unwrap();
+        return Ok(());
+    }
+}
+
+impl<F: PrimeField, const I: usize> Chip<F> for MerkleExtendedPathChip<F, I> {
+    type Config = MerklePathConfig<I>;
+
+    type Loaded = ();
+
+    fn config(&self) -> &Self::Config {
+        &self.config
+    }
+
+    fn loaded(&self) -> &Self::Loaded {
+        &()
+    }
+}
--- psc/src/lib.rs
+++ psc/src/lib.rs
@@ -1,9 +1,23 @@
 mod buckets;
+mod chip;
+mod circuit;
 mod hashes;
+mod poseidon_spec;
 use crate::buckets::Buckets;
 use crate::buckets::Node;
+use crate::circuit::MerkleExtendedPathCircuit;
 use crate::hashes::*;
+use crate::poseidon_spec::PoseidonSpec;
+use ff::Field;
 use ff::PrimeField;
+use halo2_proofs::circuit::Value;
+use halo2_proofs::pasta::{EqAffine, Fp};
+use halo2_proofs::plonk::{
+    create_proof, keygen_pk, keygen_vk, verify_proof, ProvingKey, SingleVerifier, VerifyingKey,
+};
+use halo2_proofs::poly::commitment::Params;
+use halo2_proofs::transcript::{Blake2bRead, Blake2bWrite, Challenge255};
+use rand::rngs::OsRng;
 
 /// Psc is an auxirary for a set of elements and supports to generate
 /// prove that a given element is in or not in the set.
@@ -18,23 +32,48 @@ pub struct Psc<const LE: usize, const LM: usize, const LB: usize> {
     k: Vec<u64>,
 }
 
-pub struct Witness {
+pub struct Proof {
     r: u32,
     k: Vec<u64>,
     proof: Vec<Node>,
 }
 
+pub struct ZKProof {
+    r: u32,
+    k: Vec<u64>,
+    f: Fp,
+    proof: Vec<u8>,
+}
+
 pub trait SetCommitment<const LE: usize, const LM: usize, const LB: usize> {
     type Element;
 
-    fn prove(&self, element: &[u64; LE]) -> Witness;
+    fn prove(&self, element: &[u64; LE]) -> Proof;
 
     fn verify_membership(
         element: &[u64; LE],
-        witness: &Witness,
+        witness: &Proof,
         set_commitment: &[u8; 32],
         k_commitment: &[u64; 4],
     ) -> Result<bool, String>;
+
+    fn zk_prove(
+        &self,
+        element: &[u64; LE],
+        param: &Params<EqAffine>,
+        pk: &ProvingKey<EqAffine>,
+    ) -> ZKProof;
+
+    fn zk_verify_membership(
+        element: &[u64; LE],
+        witness: &ZKProof,
+        set_commitment: &[u8; 32],
+        k_commitment: &[u64; 4],
+        param: &Params<EqAffine>,
+        vk: &VerifyingKey<EqAffine>,
+    ) -> Result<bool, String>;
+
+    fn zk_setup() -> (Params<EqAffine>, ProvingKey<EqAffine>);
 }
 
 impl<const LE: usize, const LM: usize, const LB: usize> Psc<LE, LM, LB> {
@@ -123,7 +162,7 @@ impl<const LE: usize, const LM: usize, const LB: usize> SetCommitment<LE, LM, LB
 {
     type Element = [u8; LE];
 
-    fn prove(&self, element: &[u64; LE]) -> Witness {
+    fn prove(&self, element: &[u64; LE]) -> Proof {
         let index = self
             .k
             .iter()
@@ -161,16 +200,144 @@ impl<const LE: usize, const LM: usize, const LB: usize> SetCommitment<LE, LM, LB
 
         path.reverse();
 
-        Witness {
+        Proof {
             r,
             k: self.k.clone(),
             proof: path,
         }
     }
 
+    fn zk_prove(
+        &self,
+        element: &[u64; LE],
+        param: &Params<EqAffine>,
+        pk: &ProvingKey<EqAffine>,
+    ) -> ZKProof {
+        const W: usize = 3;
+        const R: usize = W - 1;
+
+        let index = self
+            .k
+            .iter()
+            .map(|k| (highwayhash_64(*k, &element) & 1) as usize)
+            .collect::<Vec<_>>();
+
+        assert_eq!(index.len(), Self::MAX_TREE_DEPTH);
+
+        // n is depth of real tree
+        let n = self.aux.len() - 2;
+
+        let mut position = index.iter().take(n - LB).fold(0, |acc, &x| (acc << 1) + x) << LB;
+
+        // fetch the first leaf of the bucket
+        let bucket_leaf = &self.aux[0][position];
+        let r = match bucket_leaf {
+            Node::Field(_) => panic!("raw leaf should not be a filed"),
+            Node::Raw((r, _)) => r.to_owned(),
+        };
+
+        // fetch the real leaf
+        let bucket_offset = get_index(element, r, Self::BUCKET_SIZE - 1);
+        position += bucket_offset;
+
+        let mut left = vec![];
+        let mut right = vec![];
+
+        // first we input root
+        let root = self.aux.last().expect("aux should not be empty")[0].to_owned();
+
+        let mut public = vec![match root {
+            Node::Field(f) => f,
+            _ => panic!("root should be a field element"),
+        }];
+
+        let leaf = self.aux[1][position].clone();
+        for level in 0..n {
+            if position % 2 == 0 {
+                left.push(self.aux[level + 1][position].clone());
+                right.push(self.aux[level + 1][position + 1].clone());
+            } else {
+                right.push(self.aux[level + 1][position].clone());
+                left.push(self.aux[level + 1][position - 1].clone());
+            }
+            position /= 2;
+        }
+
+        let mut copy = vec![Value::known(Fp::ONE); LM - n + LB - 1];
+        copy.resize_with(LM, || Value::known(Fp::ZERO));
+        let prover_circuit = MerkleExtendedPathCircuit::<Fp, PoseidonSpec<W, R>, LM, W, R, LB>::new(
+            left.into_iter()
+                .map(|x| match x {
+                    Node::Field(f) => vec![Value::known(f)],
+                    _ => panic!("left should be a field element"),
+                })
+                .collect(),
+            right
+                .into_iter()
+                .map(|x| match x {
+                    Node::Field(f) => vec![Value::known(f)],
+                    _ => panic!("left should be a field element"),
+                })
+                .collect(),
+            copy,
+        );
+
+        // fill in the full path
+
+        for bit in index.iter() {
+            public.push(match bit {
+                0 => Fp::ZERO,
+                1 => Fp::ONE,
+                _ => panic!("bit should be 0 or 1"),
+            });
+        }
+
+        for i in (0..LB).rev() {
+            public.push(match (bucket_offset >> i) & 1 {
+                0 => Fp::ZERO,
+                1 => Fp::ONE,
+                _ => panic!("bit should be 0 or 1"),
+            }); // Use bitwise AND instead of modulo
+        }
+        public.push(match leaf {
+            Node::Field(f) => f,
+            _ => panic!("leaf should be a field element"),
+        });
+
+        public.reverse();
+
+        assert_eq![public.len(), LM + 2];
+
+        {
+            use halo2_proofs::dev::MockProver;
+            let prover = MockProver::run(15, &prover_circuit, vec![public.clone()]).unwrap();
+            prover.assert_satisfied();
+        }
+
+        let mut transcript = Blake2bWrite::<_, _, Challenge255<_>>::init(vec![]);
+
+        // Create a proof
+        create_proof(
+            param,
+            pk,
+            &[prover_circuit],
+            &[&[&public]],
+            OsRng,
+            &mut transcript,
+        )
+        .expect("proof generation failed");
+
+        ZKProof {
+            r,
+            k: self.k.clone(),
+            f: public.first().expect("public should not be empty").clone(),
+            proof: transcript.finalize(),
+        }
+    }
+
     fn verify_membership(
         element: &[u64; LE],
-        witness: &Witness,
+        witness: &Proof,
         set_commitment: &[u8; 32],
         k_commitment: &[u64; 4],
     ) -> Result<bool, String> {
@@ -235,5 +402,87 @@ impl<const LE: usize, const LM: usize, const LB: usize> SetCommitment<LE, LM, LB
         }
 
         Ok(membership)
+    }
+
+    fn zk_verify_membership(
+        element: &[u64; LE],
+        witness: &ZKProof,
+        set_commitment: &[u8; 32],
+        k_commitment: &[u64; 4],
+        param: &Params<EqAffine>,
+        vk: &VerifyingKey<EqAffine>,
+    ) -> Result<bool, String> {
+        // verify the k commitment
+        let verified_k = highwayhash_default_256(&witness.k);
+        if *k_commitment != verified_k {
+            return Err("k_commitment is not correct".to_string());
+        }
+
+        let mut public =
+            vec![Fp::from_repr(set_commitment.to_owned()).expect("Invalid set_commitment")];
+
+        for k in witness.k.iter() {
+            public.push(match highwayhash_64(*k, &element) & 1 {
+                0 => Fp::ZERO,
+                1 => Fp::ONE,
+                _ => panic!("bit should be 0 or 1"),
+            });
+        }
+
+        // compute bucket index
+
+        let pos = get_index(element, witness.r, Self::BUCKET_SIZE - 1);
+
+        // get the final path
+        for i in (0..LB).rev() {
+            public.push(match (pos >> i) & 1 {
+                0 => Fp::ZERO,
+                1 => Fp::ONE,
+                _ => panic!("bit should be 0 or 1"),
+            });
+        }
+
+        // verify the index length, now it should be n-1
+        assert_eq!(public.len(), LM + 1);
+
+        let leaf = witness.f.clone();
+        let my_leaf = poseidonhash_node(&Node::Raw((witness.r.clone(), element.to_vec())));
+        let membership = match (my_leaf, leaf.clone()) {
+            (Node::Field(input), proved) => input == proved,
+            _ => panic!("Both leaf should be a field element"),
+        };
+
+        public.push(leaf);
+
+        public.reverse();
+
+        let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(&witness.proof[..]);
+        let strategy = SingleVerifier::new(param);
+        // verify the path
+        let result = verify_proof(param, vk, strategy, &[&[&public]], &mut transcript);
+
+        match result {
+            Ok(_) => Ok(membership),
+            Err(error) => panic!("Problem opening the file: {:?}", error),
+        }
+    }
+
+    fn zk_setup() -> (Params<EqAffine>, ProvingKey<EqAffine>) {
+        const POSEIDON_DEGREE: u32 = 7;
+        const W: usize = 3;
+        const R: usize = W - 1;
+
+        let log_m = (LM as f64).log2().ceil() as u32;
+
+        let params: Params<EqAffine> = Params::new(log_m + POSEIDON_DEGREE);
+        let empty_circuit = MerkleExtendedPathCircuit::<Fp, PoseidonSpec<W, R>, LM, W, R, LB>::new(
+            vec![vec![Value::unknown(); 1]; LM],
+            vec![vec![Value::unknown(); 1]; LM],
+            vec![Value::unknown(); LM],
+        );
+
+        let vk = keygen_vk(&params, &empty_circuit).expect("keygen_vk should not fail");
+        let pk = keygen_pk(&params, vk, &empty_circuit).expect("keygen_pk should not fail");
+        return (params, pk);
     }
 }
