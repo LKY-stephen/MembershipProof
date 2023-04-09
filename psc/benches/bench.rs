f2571f0090fefb79e3a259590c299ba0f7d48fe3
Keyang.Liu <stephenkobylky@gmail.com>
2023-04-09 12:51:58 +0900

    refactor

--- psc/benches/bench.rs
+++ psc/benches/bench.rs
@@ -1,4 +1,6 @@
-use criterion::{criterion_group, criterion_main, Criterion};
+use criterion::{
+    criterion_group, criterion_main, measurement::WallTime, BenchmarkGroup, BenchmarkId, Criterion,
+};
 
 use psc::{Psc, SetCommitment};
 use rand::{seq::SliceRandom, Rng};
@@ -8,133 +10,139 @@ use std::{collections::HashSet, time::Duration};
 const LN: usize = 4;
 const LB: usize = 3;
 
-fn proof_criterion_256(c: &mut Criterion) {
+fn bench_with_m(c: &mut Criterion) {
+    let set = gen_rand_list(10);
+    let mut group = c.benchmark_group("with_changed_m");
     seq_macro::seq!(M in 32..64 {
-        generate_proof_fn::<M>(c);
+        bench_halo::<M,_>(&set, &mut group,|s: &str| BenchmarkId::new(&format!("{s} for n: 10 ", ), M));
     });
+
+    group.finish();
 }
 
-fn generate_proof_fn<const M: usize>(c: &mut Criterion) {
-    for n in 10..=20 {
-        let (set, psc) = prepare_set::<M>(n);
-        let element = set.choose(&mut rand::thread_rng()).expect("set is empty");
-        let (param, pk) = Psc::<4, 32, 3>::zk_setup();
-
-        // bench setup
-        c.bench_function(
-            &format!("Setup for prove key and verification key for n:{n} m: {M}"),
-            |b| {
-                b.iter(|| {
-                    Psc::<4, 32, 3>::zk_setup();
-                })
-            },
-        );
-
-        let proof = psc.zk_prove(element, &param, &pk);
-
-        // bench membership proof
-        c.bench_function(&format!("membership proof for n:{n} m: {M}"), |b| {
-            b.iter(|| {
-                psc.zk_prove(element, &param, &pk);
-            })
+fn bench_with_n(c: &mut Criterion) {
+    let mut group = c.benchmark_group("with_changed_n");
+    for n in 5..=20 {
+        let set = gen_rand_list(n);
+        bench_halo::<32, _>(&set, &mut group, |s: &str| {
+            BenchmarkId::new(&format!("{s} for m: 32 ",), n)
         });
-
-        println!("proof size is {} bytes", proof.len());
-
-        let (set_commitment, k_commitment) = psc.get_commitment();
-
-        let result = Psc::<LN, M, LB>::zk_verify_membership(
-            element,
-            &proof,
-            &set_commitment,
-            &k_commitment,
-            &param,
-            pk.get_vk(),
-        );
-        assert_eq!(result.is_ok(), true);
-        assert_eq!(result.unwrap(), true);
-
-        c.bench_function(&format!("membership verification for n:{n} m: {M}"), |b| {
-            b.iter(|| {
-                Psc::<LN, M, LB>::zk_verify_membership(
-                    element,
-                    &proof,
-                    &set_commitment,
-                    &k_commitment,
-                    &param,
-                    pk.get_vk(),
-                )
-                .unwrap();
-            })
-        });
-
-        let fake_element = [1, 1, 1, 0];
-        let proof = psc.zk_prove(&fake_element, &param, &pk);
-
-        // bench non-membership proof
-        c.bench_function(&format!("non-membership proof for n:{n} m: {M}"), |b| {
-            b.iter(|| {
-                psc.zk_prove(&fake_element, &param, &pk);
-            })
-        });
-
-        let (set_commitment, k_commitment) = psc.get_commitment();
-        let result = Psc::<LN, M, LB>::zk_verify_membership(
-            &fake_element,
-            &proof,
-            &set_commitment,
-            &k_commitment,
-            &param,
-            pk.get_vk(),
-        );
-        // bench non-membership proof
-        c.bench_function(
-            &format!("non-membership verification for n:{n} m: {M}"),
-            |b| {
-                b.iter(|| {
-                    Psc::<LN, M, LB>::zk_verify_membership(
-                        &fake_element,
-                        &proof,
-                        &set_commitment,
-                        &k_commitment,
-                        &param,
-                        pk.get_vk(),
-                    )
-                    .unwrap();
-                })
-            },
-        );
-
-        assert_eq!(result.is_ok(), true);
-        assert_eq!(result.unwrap(), false);
     }
+
+    group.finish();
 }
 
-fn prepare_set<const M: usize>(n: usize) -> (Vec<[u64; LN]>, Psc<LN, M, LB>) {
+fn bench_halo<const M: usize, F: Fn(&str) -> BenchmarkId>(
+    set: &Vec<[u64; LN]>,
+    group: &mut BenchmarkGroup<WallTime>,
+    gen_id: F,
+) {
+    let psc = Psc::<LN, M, LB>::new(&set);
+    let element = set.choose(&mut rand::thread_rng()).expect("set is empty");
+    let (param, pk) = Psc::<LN, M, LB>::zk_setup();
+
+    // bench setup
+    group.bench_function(gen_id("set up"), |b| {
+        b.iter(|| {
+            Psc::<LN, M, LB>::zk_setup();
+        })
+    });
+
+    let proof = psc.zk_prove_halo(element, &param, &pk);
+
+    // bench membership proof
+    group.bench_function(gen_id("membership proof"), |b| {
+        b.iter(|| {
+            psc.zk_prove_halo(element, &param, &pk);
+        })
+    });
+
+    println!("proof size is {} bytes", proof.len());
+
+    let (set_commitment, k_commitment) = psc.get_commitment();
+
+    let result = Psc::<LN, M, LB>::zk_verify_halo(
+        element,
+        &proof,
+        &set_commitment,
+        &k_commitment,
+        &param,
+        pk.get_vk(),
+    );
+    assert_eq!(result.is_ok(), true);
+    assert_eq!(result.unwrap(), true);
+
+    group.bench_function(gen_id("membership verification"), |b| {
+        b.iter(|| {
+            Psc::<LN, M, LB>::zk_verify_halo(
+                element,
+                &proof,
+                &set_commitment,
+                &k_commitment,
+                &param,
+                pk.get_vk(),
+            )
+            .unwrap();
+        })
+    });
+
+    let fake_element = [1, 1, 1, 0];
+    let proof = psc.zk_prove_halo(&fake_element, &param, &pk);
+
+    // bench non-membership proof
+    group.bench_function(gen_id("non-membership proof"), |b| {
+        b.iter(|| {
+            psc.zk_prove_halo(&fake_element, &param, &pk);
+        })
+    });
+
+    let (set_commitment, k_commitment) = psc.get_commitment();
+    let result = Psc::<LN, M, LB>::zk_verify_halo(
+        &fake_element,
+        &proof,
+        &set_commitment,
+        &k_commitment,
+        &param,
+        pk.get_vk(),
+    );
+    // bench non-membership proof
+    group.bench_function(gen_id("non-membership verification"), |b| {
+        b.iter(|| {
+            Psc::<LN, M, LB>::zk_verify_halo(
+                &fake_element,
+                &proof,
+                &set_commitment,
+                &k_commitment,
+                &param,
+                pk.get_vk(),
+            )
+            .unwrap();
+        })
+    });
+
+    assert_eq!(result.is_ok(), true);
+    assert_eq!(result.unwrap(), false);
+}
+
+fn gen_rand_list(n: usize) -> Vec<[u64; LN]> {
     let set_size = 1 << n;
     println!("Generate test set");
-    let set = gen_rand_list(set_size).into_iter().collect::<Vec<_>>();
-
-    let psc = Psc::<LN, M, LB>::new(&set);
-
-    println!("Commit scheme completed");
-    return (set, psc);
-}
-
-fn gen_rand_list(num: usize) -> HashSet<[u64; LN]> {
     let mut rng = rand::thread_rng();
-    let mut set = HashSet::with_capacity(num);
+    let mut set = HashSet::with_capacity(set_size);
 
-    for _ in 0..num {
-        set.insert([rng.gen::<u64>(), rng.gen::<u64>(), rng.gen::<u64>(), 1]);
+    let mut filled = 0;
+    while filled < set_size {
+        if set.insert([rng.gen::<u64>(), rng.gen::<u64>(), rng.gen::<u64>(), 1]) {
+            filled += 1;
+        }
     }
 
-    return set;
+    return set.into_iter().collect::<Vec<_>>();
 }
 
 criterion_group! {
     name = benches;
-    config = Criterion::default().measurement_time(Duration::from_secs(30)).sample_size(10);
-    targets = proof_criterion_256
+    config = Criterion::default().measurement_time(Duration::from_secs(50)).sample_size(10);
+    targets = bench_with_m, bench_with_n
 }
 criterion_main!(benches);
